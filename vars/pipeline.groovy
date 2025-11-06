9e49d33fb36ae3b6660d0db5d973db40ad50fcdf
lokesh udatha <138110408+lokeshudatha@users.noreply.github.com>
2025-11-06 11:10:49 +0530

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,109 +1,31 @@
-import com.lokesh.build.cal 
+import com.udatha.build.Cal 
 
-def call(Map pipelineParams) {
-    Cal one = new Cal(this)
+def call(Map pipelineParams){
+    Cal cal = new Cal(this)
 }
 
 pipeline {
     agent any
-    environment {
-        myapp = "${pipelineParams.lokeshapp}"
+    environment{
+        APP = "${pipelineParams.myapp}"
     }
-    
     parameters {
-        string(name: 'USER', defaultValue: 'lokesh udatha', description: 'Enter your full name here: ')
-        booleanParam(name: 'Run', defaultValue: true, description: 'Running your test cases')
-        choice(name: 'Env', choices: ['build', 'test', 'prod'], description: 'Select one environment here: ')
-    }
-
-    tools {
-        maven 'Maven3'
-        jdk 'JDK17'
-    }
-
-    options {
-        timeout(time: 02, unit: 'SECONDS')
-    }
-
-    triggers {
-        cron('*/1 * * * *')
-        pollSCM('*/1 * * * *')
+        string(name: 'user', defaultValue: 'lokesh', description: 'Enter your name here:')
     }
     stages {
-        stage('Input Approval') {
-            steps {
-                input message: 'Do you want to proceed?', 
-                ok: 'yes, continue'
-                echo "stage one is complate"
-            }
-
-        }
-        stage('Setup') {
-            steps{
-                sh 'mvn --version'
-                sh 'java -version'
-            }
-        }
-        stage('Build and Test') {
-            parallel {
-                stage('Build') {
-                    steps {
-                        script {
-                            try {
-                                echo "if  ${env.myapp} is correct print the below command"
-                                sh 'mvn clean package'
-                            } catch {
-                                error ("stopping this pipeline here.")
-                            } finally {
-                                echo "Build stage is complated"
-                            }
-                        }
-                    }
-                    stage('Test') {
-                        when {
-                            expression {
-                                return params.Run == true
-                            }
-                        }
-                        steps {
-                            echo "running unit tests..."
-                            sh 'mvn test'
-                        }
-                    }
-                }
-            }
-        }
-        stage('Deploy') {
+        stage('para') {
             when {
-                allOf {
-                    expression {
-                        return params.Env == 'build'
-                    }
-                    expression {
-                        return params.Env == 'test'
-                    }
+                expression {
+                    return params.user == 'lokesh'
                 }
             }
             steps {
-                script {
-                    if (params.Env == 'prod') {
-                        echo "Producation is running proper"
-                    } else {
-                        echo "some errors in producation"
-                    }
-                }
+                sh '''
+                    mkdir -p udatha
+                    cd udatha
+                    echo "success"
+                '''
             }
         }
-
     }
-    post {
-        success {
-            echo "Pipeline is working good"
-        } failure {
-            echo "Pipeline is not working properlly"
-        } always {
-            echo "Pipeline execution is complated"
-        }
-    }
-
 }
